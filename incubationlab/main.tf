e2c8235154e27b93b292628d96f3de2ad92a8bc7
Prayashi_Upadhyay <prayashi_upadhyay@epam.com>
2022-06-03 13:16:21 +0530

    change

--- incubationlab/main.tf
+++ incubationlab/main.tf
@@ -49,9 +49,7 @@ module "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location = var.location
   acr_name = "labacrpu"
-  tags = {
-    environment = "dev"
-  }
+
 }
 
 #kubernetes cluster
