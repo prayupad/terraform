291bccf088e339c7eea85a68829e7efb73a8bb2c
Prayashi_Upadhyay <prayashi_upadhyay@epam.com>
2022-06-08 01:24:57 +0530

    change in subnet address range

--- incubationlab/modules/vnet/variables.tf
+++ incubationlab/modules/vnet/variables.tf
@@ -32,7 +32,7 @@ variable "aks_subnet" {
 }
 variable "akssubnet_address_range" {
   type        = list(string)
-  default = [ "10.240.1.0/16" ]
+  default = [ "10.240.0.0/16" ]
 }
 
 variable "subnet_name" {
@@ -43,5 +43,5 @@ variable "subnet_name" {
 
 variable "subnet_address_range" {
   type        = list(string)
-  default = [ "10.240.2.0/16" ]
+  default = [ "10.241.0.0/16" ]
 }
