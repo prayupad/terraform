b458e95d4db44141abc5328a688dee2d00e72df8
Prayashi Upadhyay <Prayashi_Upadhyay@epam.com>
2022-06-10 05:25:37 +0000

    Updated aks.tf

--- incubationlab/modules/aks/aks.tf
+++ incubationlab/modules/aks/aks.tf
@@ -58,14 +58,14 @@ resource "azurerm_public_ip" "kubernetes" {
 
 }
 
-/*
+
 resource "azurerm_role_assignment" "role_network" {
   scope                            = var.vnet_id
   role_definition_name             = "Network Contributor"
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
-*/
+
 
 
 
